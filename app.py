23328a870b0d73368dcbe2f5b9946d35f09610ad
Andreim1998 <savin.andrey98@gmail.com>
2021-08-09 19:03:27 +0000

    Added more register options

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import (Flask, flash, render_template,redirect, request, session, url_for)
+from flask import (Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
