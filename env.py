182418775ea78c48d37417670f9574f86faebf95
Andreim1998 <savin.andrey98@gmail.com>
2021-08-10 19:19:32 +0000

    Added procfile changes

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "ruygVwANK2TUDe8k")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:ruygVwANK2TUDe8k@myfirstcluster.3g8hc.mongodb.net/task_manager?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")
