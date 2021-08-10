ea4c6469b6e3128cc3b80b580478209a5ec6ceb3
Andreim1998 <savin.andrey98@gmail.com>
2021-08-10 18:53:02 +0000

    Add session user profile

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def register():
         #put  the new into 'session' cookie
         session['user'] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
+
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -56,9 +58,10 @@ def login():
 
         if existing_user:
             #ensure hashed password matches user inputs
-            if check_password_hash(existing_user["passowrd"], request.form.get("passowrd")):
+            if check_password_hash(existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 #invalid passowrd match
                 flash("Incorecct Username and/or Password")
@@ -70,6 +73,15 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    #grab the sessions user's username form db
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
